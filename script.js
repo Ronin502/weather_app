2dd42d15f5f24e5bec41bd67fd88498604c7a0ae
Jesse Brooks <ronin702502@gmail.com>
2022-12-15 16:46:22 -0500

    name and icon working

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ let weather = {
             + this.apiKey
         )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
     }
 };
