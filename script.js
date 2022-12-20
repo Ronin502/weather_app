d2c77b984dd7ddfcb1d48983df1765b1e3f09f23
Jesse Brooks <ronin702502@gmail.com>
2022-12-20 17:18:36 -0500

    very rough autocomplete

--- script.js
+++ script.js
@@ -43,3 +43,37 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
 })
 
 weather.fetchWeather("Louisville");
+
+const data = ["Las Vegas", "San Sebastian", "Tokyo", "Tibet", "Lexington", "London", "Seoul", "Naha", "Nepal", "Santa Monica", "San Diego"];
+
+const autocomplete = document.getElementById("autocomplete");
+const resultsHTML = document.getElementById("results");
+
+autocomplete.oninput = function () {
+    let results = [];
+    const userInput = this.value;
+    resultsHTML.innerHTML = "";
+    if (userInput.length > 0) {
+      results = getResults(userInput);
+      resultsHTML.style.display = "block";
+      for (i = 0; i < results.length; i++) {
+        resultsHTML.innerHTML += "<li>" + results[i] + "</li>";
+      }
+    }
+};
+
+function getResults(input) {
+    const results = [];
+    for (i = 0; i < data.length; i++) {
+      if (input === data[i].slice(0, input.length)) {
+        results.push(data[i]);
+      }
+    }
+    return results;
+};
+
+resultsHTML.onclick = function (event) {
+    const setValue = event.target.innerText;
+    autocomplete.value = setValue;
+    this.innerHTML = "";
+};
